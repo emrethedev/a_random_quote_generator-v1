a58975517966e3675efdd38731a68909b22bc782
Emre <61990404+emrethedev@users.noreply.github.com>
2020-03-14 19:58:05 -0700

    Introducing the bg-color changer

--- js/script.js
+++ js/script.js
@@ -57,6 +57,46 @@ const getRandomQuote = () => {
 
 console.log(getRandomQuote());
 
+let pickedColors = [
+    {
+      color: "#F83839",
+    },
+
+    {
+      color: "#513B41",
+    },
+
+    {
+      color: "#7FE5F0",
+    },
+
+    {
+      color: "#C8F4F9",
+    },
+
+    {
+      color: "#EFEBE2",
+    },
+
+    {
+      color: "#B175FF",
+    },
+
+    {
+      color: "#AB0552",
+    },
+];
+console.log(pickedColors);
+console.log(pickedColors.length);
+
+// color generation function
+const randColorGen = ( ) => {
+    let colorGen = Math.floor(Math.random() * pickedColors.length);
+    let colorMaker = document.querySelector("body");
+    return colorMaker.style.backgroundColor = pickedColors[colorGen].color;
+};
+
+
 /***
  * `printQuote` function
 ***/
@@ -74,7 +114,8 @@ const printQuote = () => {
     }
     quoteMsg += `</p>`;
     console.log(quoteMsg);
-    return document.getElementById('quote-box').innerHTML = quoteMsg;
+    randColorGen();
+    return document.getElementById('quote-box').innerHTML = `${quoteMsg}`;
 };
 /***
  * click event listener for the print quote button
